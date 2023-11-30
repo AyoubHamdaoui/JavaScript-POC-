0b54e2c0cb7ada63c3df3151f139985d08f73b90
AyoubHamdaoui <ayoubhamdaoui94@gmail.com>
2023-11-30 03:31:10 +0100

    TP 1 : - déclaration d'une variable JS .

---
+++ tp1.js
@@ -0,0 +1,11 @@
+// Partie pratique JS 
+// Définition d'une varible en JS 
+
+/* const pour déclarer une variable  constante */
+const Prenom="Ayoub"
+console.log(Prenom)
+
+
+/* let pour déclarer un variable  non constante */
+let age=24
+console.log(age)
